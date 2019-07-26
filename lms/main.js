1a96c592b4a78e2218fdc11598338d5616f7f782
Mark Tushemereirwe <tushmark@Marks-MacBook-Air.local>
2019-07-26 05:52:06 -0400

    update cors

--- lms/main.js
+++ lms/main.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });

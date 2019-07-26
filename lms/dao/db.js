0a6b2b708ce96cd28c73bcb1a67bf53e99f46892
Mark Tushemereirwe <tushmark@Marks-MacBook-Air.local>
2019-07-26 16:21:39 -0400

    update connection

--- lms/dao/db.js
+++ lms/dao/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : '127.0.0.1',
+    host     : 'mydbinstance.cslcbvxulvmd.us-east-2.rds.amazonaws.com',
     user     : 'root',
     password : 'kingtush7',
     database : 'lms'
